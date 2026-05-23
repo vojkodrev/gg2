4eddc19be2923a076648c8f2630001c9d819651e
Vojko Drev <vojkodrev@gmail.com>
2026-05-23 07:45:25 +0700

    Update keyboard state with modifiers and digit keys

--- gg2/structs/core/KeyboardState.h
+++ gg2/structs/core/KeyboardState.h
@@ -3,4 +3,6 @@
 struct KeyboardState
 {
     bool w, a, s, d;
+    bool ctrl, shift, alt;
+    bool digit0, digit1, digit2, digit3, digit4, digit5, digit6, digit7, digit8, digit9;
 };
--- gg2/systems/core/input/UpdateKeyboardStateSystem.cpp
+++ gg2/systems/core/input/UpdateKeyboardStateSystem.cpp
@@ -8,4 +8,17 @@ void updateKeyboardStateSystem(Context &ctx)
     ctx.keyboard.a = keys[SDL_SCANCODE_A];
     ctx.keyboard.s = keys[SDL_SCANCODE_S];
     ctx.keyboard.d = keys[SDL_SCANCODE_D];
+    ctx.keyboard.ctrl = keys[SDL_SCANCODE_LCTRL] || keys[SDL_SCANCODE_RCTRL];
+    ctx.keyboard.shift = keys[SDL_SCANCODE_LSHIFT] || keys[SDL_SCANCODE_RSHIFT];
+    ctx.keyboard.alt = keys[SDL_SCANCODE_LALT] || keys[SDL_SCANCODE_RALT];
+    ctx.keyboard.digit0 = keys[SDL_SCANCODE_0];
+    ctx.keyboard.digit1 = keys[SDL_SCANCODE_1];
+    ctx.keyboard.digit2 = keys[SDL_SCANCODE_2];
+    ctx.keyboard.digit3 = keys[SDL_SCANCODE_3];
+    ctx.keyboard.digit4 = keys[SDL_SCANCODE_4];
+    ctx.keyboard.digit5 = keys[SDL_SCANCODE_5];
+    ctx.keyboard.digit6 = keys[SDL_SCANCODE_6];
+    ctx.keyboard.digit7 = keys[SDL_SCANCODE_7];
+    ctx.keyboard.digit8 = keys[SDL_SCANCODE_8];
+    ctx.keyboard.digit9 = keys[SDL_SCANCODE_9];
 }
