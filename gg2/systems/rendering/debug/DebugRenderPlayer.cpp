8af43c7f860f1be2a4560819c1e07fa5034d08b4
Vojko Drev <vojkodrev@gmail.com>
2026-05-24 10:47:22 +0700

    Add anchor AABB helpers and use in player debug render

--- gg2/systems/rendering/debug/DebugRenderPlayer.cpp
+++ gg2/systems/rendering/debug/DebugRenderPlayer.cpp
@@ -1,12 +1,12 @@
 #include "DebugRenderPlayer.h"
 #include "RenderColBox.h"
+#include "../../../utils/collision/EntityAnchorAABBPlayer.h"
 #include "../../../utils/collision/EntityColAABBPlayer.h"
 
 void debugRenderPlayer(const Context &ctx, SDL_FPoint off, const SDL_FRect &screen)
 {
     SDL_Renderer *renderer = ctx.renderer;
     const auto &player = ctx.data.player;
-    const int f = player.animation.frameIndex[0];
 
     SDL_FRect col = entityColAABB(player);
     if (col.w > 0.0f && col.h > 0.0f)
@@ -15,17 +15,9 @@ void debugRenderPlayer(const Context &ctx, SDL_FPoint off, const SDL_FRect &scre
         renderColBox(renderer, col, off, screen);
     }
 
-    SDL_FRect anchor = {
-        player.animation.frame.anchor.offX[0][f],
-        player.animation.frame.anchor.offY[0][f],
-        player.animation.frame.anchor.w[0][f],
-        player.animation.frame.anchor.h[0][f]
-    };
+    SDL_FRect anchor = entityAnchorAABB(player);
     if (anchor.w <= 0.0f || anchor.h <= 0.0f)
         return;
-
-    anchor.x = player.position.x[0] + anchor.x;
-    anchor.y = player.position.y[0] + anchor.y;
 
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
     renderColBox(renderer, anchor, off, screen);
