3b34d01a36d90adea742e5566c42d8bfc2ef02f7
Vojko Drev <vojkodrev@gmail.com>
2026-05-25 11:59:42 +0700

    Add marker-aware entity base loading for objects

--- gg2/systems/tilemap/LoadEntityBase.h
+++ gg2/systems/tilemap/LoadEntityBase.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "../../structs/tilemap/TileMapProperties.h"
+#include "DecodeGridIndex.h"
+#include "IsMarker.h"
 #include "LoadTileAnimation.h"
 #include "properties/GetTileFloatProp.h"
+#include "properties/GetTileIntProp.h"
 #include "properties/GetTileStringProp.h"
 #include <cstdint>
 #include <tmxlite/Tileset.hpp>
@@ -12,14 +15,55 @@ inline void loadEntityBase(
     uint32_t parentEntityIdx,
     const tmx::Tileset &tileset,
     uint32_t entityTileIdx,
-    const TileMapProperties &props)
+    const TileMapProperties &props,
+    uint32_t tileArrayIdx = UINT32_MAX)
 {
-    loadTileAnimation(entityData.animation, parentEntityIdx, tileset, entityTileIdx, props);
+    bool marker = tileArrayIdx != UINT32_MAX && isMarker(tileset, entityTileIdx);
+    if (marker)
+    {
+        entityData.animation.frame.src.x[parentEntityIdx][0] = getTileIntProp(tileset, entityTileIdx, "x");
+        entityData.animation.frame.src.y[parentEntityIdx][0] = getTileIntProp(tileset, entityTileIdx, "y");
+        entityData.animation.frame.src.w[parentEntityIdx][0] = getTileIntProp(tileset, entityTileIdx, "w");
+        entityData.animation.frame.src.h[parentEntityIdx][0] = getTileIntProp(tileset, entityTileIdx, "h");
+        float colOffX = (float)getTileIntProp(tileset, entityTileIdx, "colOffX");
+        float colOffY = (float)getTileIntProp(tileset, entityTileIdx, "colOffY");
+        float colW = (float)getTileIntProp(tileset, entityTileIdx, "colW");
+        float colH = (float)getTileIntProp(tileset, entityTileIdx, "colH");
+        float w = (float)getTileIntProp(tileset, entityTileIdx, "w");
+        float h = (float)getTileIntProp(tileset, entityTileIdx, "h");
+        SDL_Point grid = decodeGridIndex((int)tileArrayIdx, props.mapW);
+        float gridX = (float)(grid.x * props.dstTileW);
+        float gridY = (float)(grid.y * props.dstTileH);
 
-    entityData.position.initialW[parentEntityIdx] = props.srcTileW;
-    entityData.position.initialH[parentEntityIdx] = props.srcTileH;
-    entityData.position.w[parentEntityIdx] = entityData.position.initialW[parentEntityIdx];
-    entityData.position.h[parentEntityIdx] = entityData.position.initialH[parentEntityIdx];
+        entityData.position.x[parentEntityIdx] = gridX + (props.dstTileW - colW) / 2.0f - colOffX;
+        entityData.position.y[parentEntityIdx] = gridY + props.dstTileH - colOffY - colH;
+        entityData.position.initialW[parentEntityIdx] = w;
+        entityData.position.initialH[parentEntityIdx] = h;
+        entityData.position.w[parentEntityIdx] = entityData.position.initialW[parentEntityIdx];
+        entityData.position.h[parentEntityIdx] = entityData.position.initialH[parentEntityIdx];
+        entityData.animation.frame.collision.initialOffX[parentEntityIdx][0] = colOffX;
+        entityData.animation.frame.collision.initialOffY[parentEntityIdx][0] = colOffY;
+        entityData.animation.frame.collision.initialW[parentEntityIdx][0] = colW;
+        entityData.animation.frame.collision.initialH[parentEntityIdx][0] = colH;
+        entityData.animation.frame.collision.offX[parentEntityIdx][0] = colOffX;
+        entityData.animation.frame.collision.offY[parentEntityIdx][0] = colOffY;
+        entityData.animation.frame.collision.w[parentEntityIdx][0] = colW;
+        entityData.animation.frame.collision.h[parentEntityIdx][0] = colH;
+        entityData.animation.animationType[parentEntityIdx] = AnimationType::None;
+        entityData.animation.frameCount[parentEntityIdx] = 1;
+        entityData.animation.frame.frameDuration[parentEntityIdx][0] = 0;
+        entityData.animation.frameIndex[parentEntityIdx] = 0;
+        entityData.animation.animationStartTime[parentEntityIdx] = 0;
+        entityData.animation.animationStopTime[parentEntityIdx] = 0;
+    }
+    else
+    {
+        loadTileAnimation(entityData.animation, parentEntityIdx, tileset, entityTileIdx, props);
+        entityData.position.initialW[parentEntityIdx] = props.srcTileW;
+        entityData.position.initialH[parentEntityIdx] = props.srcTileH;
+        entityData.position.w[parentEntityIdx] = entityData.position.initialW[parentEntityIdx];
+        entityData.position.h[parentEntityIdx] = entityData.position.initialH[parentEntityIdx];
+    }
 
     entityData.scale[parentEntityIdx] = getTileFloatProp(tileset, entityTileIdx, "scale", 1.0f);
     entityData.rotation.initialRotate[parentEntityIdx] = getTileFloatProp(tileset, entityTileIdx, "rotate", 0.0f);
--- gg2/systems/tilemap/LoadObjects.cpp
+++ gg2/systems/tilemap/LoadObjects.cpp
@@ -1,10 +1,8 @@
 #include "LoadObjects.h"
 #include "FindLayer.h"
+#include "LoadEntityBase.h"
+#include "LoadEntityPosition.h"
 #include "IsMarker.h"
-#include "properties/GetTileFloatProp.h"
-#include "properties/GetTileIntProp.h"
-#include "LoadTileAnimation.h"
-#include "DecodeGridIndex.h"
 #include <tmxlite/TileLayer.hpp>
 
 void loadObjects(Context &ctx, const tmx::Map &map, const tmx::Tileset &tileset)
@@ -19,56 +17,8 @@ void loadObjects(Context &ctx, const tmx::Map &map, const tmx::Tileset &tileset)
             continue;
         uint32_t n = object.objectCount++;
         uint32_t idx = objectTiles[i].ID - props.firstGid;
-        float scale = getTileFloatProp(tileset, idx, "scale", 1.0f);
-        object.scale[n] = scale;
-        if (isMarker(tileset, idx))
-        {
-            object.animation.frame.src.x[n][0] = getTileIntProp(tileset, idx, "x");
-            object.animation.frame.src.y[n][0] = getTileIntProp(tileset, idx, "y");
-            object.animation.frame.src.w[n][0] = getTileIntProp(tileset, idx, "w");
-            object.animation.frame.src.h[n][0] = getTileIntProp(tileset, idx, "h");
-            float colOffX = (float)getTileIntProp(tileset, idx, "colOffX");
-            float colOffY = (float)getTileIntProp(tileset, idx, "colOffY");
-            float colW = (float)getTileIntProp(tileset, idx, "colW");
-            float colH = (float)getTileIntProp(tileset, idx, "colH");
-            float w = (float)getTileIntProp(tileset, idx, "w");
-            float h = (float)getTileIntProp(tileset, idx, "h");
-            SDL_Point grid = decodeGridIndex(i, props.mapW);
-            float gridX = (float)(grid.x * props.dstTileW);
-            float gridY = (float)(grid.y * props.dstTileH);
-
-            object.position.x[n] = gridX + (props.dstTileW - colW) / 2.0f - colOffX;
-            object.position.y[n] = gridY + props.dstTileH - colOffY - colH;
-            object.position.initialW[n] = w;
-            object.position.initialH[n] = h;
-            object.position.w[n] = object.position.initialW[n];
-            object.position.h[n] = object.position.initialH[n];
-            object.animation.frame.collision.initialOffX[n][0] = colOffX;
-            object.animation.frame.collision.initialOffY[n][0] = colOffY;
-            object.animation.frame.collision.initialW[n][0] = colW;
-            object.animation.frame.collision.initialH[n][0] = colH;
-            object.animation.frame.collision.offX[n][0] = colOffX;
-            object.animation.frame.collision.offY[n][0] = colOffY;
-            object.animation.frame.collision.w[n][0] = colW;
-            object.animation.frame.collision.h[n][0] = colH;
-            object.animation.animationType[n] = AnimationType::None;
-            object.animation.frameCount[n] = 1;
-            object.animation.frame.frameDuration[n][0] = 0;
-            object.animation.frameIndex[n] = 0;
-            object.animation.animationStartTime[n] = 0;
-            object.animation.animationStopTime[n] = 0;
-        }
-        else
-        {
-            loadTileAnimation(object.animation, n, tileset, idx, props);
-
-            SDL_Point grid = decodeGridIndex(i, props.mapW);
-            object.position.x[n] = grid.x * props.dstTileW;
-            object.position.y[n] = grid.y * props.dstTileH;
-            object.position.initialW[n] = props.dstTileW;
-            object.position.initialH[n] = props.dstTileH;
-            object.position.w[n] = object.position.initialW[n];
-            object.position.h[n] = object.position.initialH[n];
-        }
+        loadEntityBase(object.base, n, tileset, idx, props, i);
+        if (!isMarker(tileset, idx))
+            loadEntityPosition(object.base.position, n, i, props);
     }
 }
