b2ca268229ac6fb8f978e4ef3eec4e2cf2ee6e34
Vojko Drev <vojkodrev@gmail.com>
2026-06-04 12:09:01 +0700

    Refactor equipment facing dirty updates

--- gg2/systems/gameplay/flipx/EquipmentFacingSystem.cpp
+++ gg2/systems/gameplay/flipx/EquipmentFacingSystem.cpp
@@ -1,45 +1,29 @@
 #include "EquipmentFacingSystem.h"
-#include "UpdateEntityFlipByParentFacing.h"
+#include "UpdateEntityFacingDirtyByParentFacing.h"
 #include <cstdint>
 
 void equipmentFacingSystem(Context &ctx)
 {
-    auto &playerWeaponFacing = ctx.data.player.equipment.weapon.base.facing;
-    playerWeaponFacing.dirty[0] = false;
-    bool playerWeaponFlipX = playerWeaponFacing.flipX[0];
-    updateEntityFlipByParentFacing(
+    updateEntityFacingDirtyByParentFacing(
         ctx.data.player.base.facing,
-        playerWeaponFacing,
+        ctx.data.player.equipment.weapon.base.facing,
         0);
-    playerWeaponFacing.dirty[0] = playerWeaponFacing.flipX[0] != playerWeaponFlipX;
 
-    auto &playerAmmoFacing = ctx.data.player.equipment.ammo.base.facing;
-    playerAmmoFacing.dirty[0] = false;
-    bool playerAmmoFlipX = playerAmmoFacing.flipX[0];
-    updateEntityFlipByParentFacing(
+    updateEntityFacingDirtyByParentFacing(
         ctx.data.player.base.facing,
-        playerAmmoFacing,
+        ctx.data.player.equipment.ammo.base.facing,
         0);
-    playerAmmoFacing.dirty[0] = playerAmmoFacing.flipX[0] != playerAmmoFlipX;
 
     for (uint32_t i = 0; i < ctx.data.npc.npcCount; i++)
     {
-        auto &npcWeaponFacing = ctx.data.npc.equipment.weapon.base.facing;
-        npcWeaponFacing.dirty[i] = false;
-        bool npcWeaponFlipX = npcWeaponFacing.flipX[i];
-        updateEntityFlipByParentFacing(
+        updateEntityFacingDirtyByParentFacing(
             ctx.data.npc.base.facing,
-            npcWeaponFacing,
+            ctx.data.npc.equipment.weapon.base.facing,
             i);
-        npcWeaponFacing.dirty[i] = npcWeaponFacing.flipX[i] != npcWeaponFlipX;
 
-        auto &npcAmmoFacing = ctx.data.npc.equipment.ammo.base.facing;
-        npcAmmoFacing.dirty[i] = false;
-        bool npcAmmoFlipX = npcAmmoFacing.flipX[i];
-        updateEntityFlipByParentFacing(
+        updateEntityFacingDirtyByParentFacing(
             ctx.data.npc.base.facing,
-            npcAmmoFacing,
+            ctx.data.npc.equipment.ammo.base.facing,
             i);
-        npcAmmoFacing.dirty[i] = npcAmmoFacing.flipX[i] != npcAmmoFlipX;
     }
 }
