9177ee78fcc062b96a4412a6f5237bb7f1ad0662
Vojko Drev <vojkodrev@gmail.com>
2026-06-09 15:10:28 +0700

    Keep AStar debug rectangles visible

--- gg2/systems/rendering/debug/DebugRenderAstar.cpp
+++ gg2/systems/rendering/debug/DebugRenderAstar.cpp
@@ -11,9 +11,12 @@ void debugRenderAstar(const Context &ctx)
     const SDL_FPoint off = ctx.data.camera.offset;
     const SDL_FRect screen = ctx.data.camera.screen;
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    for (int i = 0; i < ctx.astarPool.pool.count; i++)
+    for (int i = 0; i < MAX_ASTARS; i++)
     {
         const AStarContext &a = ctx.astarPool.ctx[i];
+        if (a.searchW <= 0 || a.searchH <= 0)
+            continue;
+
         SDL_FRect rect = { (float)a.searchX + off.x, (float)a.searchY + off.y, (float)a.searchW, (float)a.searchH };
         if (SDL_HasRectIntersectionFloat(&rect, &screen))
             SDL_RenderRect(renderer, &rect);
