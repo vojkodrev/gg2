80d2088e5f8e3d5f275e302f364ab26d7851e20e
Vojko Drev <vojkodrev@gmail.com>
2026-06-09 15:45:05 +0700

    Spawn projectile effects from ammo

--- gg2/systems/gameplay/projectile/SpawnProjectileSystem.cpp
+++ gg2/systems/gameplay/projectile/SpawnProjectileSystem.cpp
@@ -1,9 +1,21 @@
 #include "SpawnProjectileSystem.h"
+#include "../../../utils/pool/PoolAlloc.h"
+#include "../../../utils/entity/CopyEntityBaseSlot.h"
 
 void spawnProjectileSystem(Context &ctx)
 {
     if (ctx.data.action.digit1Released)
     {
-        (void)ctx;
+        const auto &playerAmmo = ctx.data.player.equipment.ammo.base;
+        const auto &playerWeapon = ctx.data.player.equipment.weapon;
+        if (!playerWeapon.hasAmmo[0])
+            return;
+
+        int effectIndex = poolAlloc(ctx.data.effect.pool);
+        if (effectIndex == -1)
+            return;
+
+        copyEntityBaseSlot(playerAmmo, 0, ctx.data.effect.base, effectIndex);
+        ctx.data.player.equipment.weapon.showAmmo[0] = false;
     }
 }
---
+++ gg2/utils/entity/CopyEntityBaseSlot.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "../../structs/core/EntityBase.h"
+#include "../../structs/core/constants/AnimationConstants.h"
+
+template<int TFromCount, int TToCount>
+inline void copyEntityBaseSlot(
+    const EntityBase<TFromCount> &from,
+    int fromIndex,
+    EntityBase<TToCount> &to,
+    int toIndex)
+{
+    to.animation.animationType[toIndex] = from.animation.animationType[fromIndex];
+    to.animation.frameCount[toIndex] = from.animation.frameCount[fromIndex];
+    to.animation.frameIndex[toIndex] = from.animation.frameIndex[fromIndex];
+    to.animation.animationStartTime[toIndex] = from.animation.animationStartTime[fromIndex];
+    to.animation.animationStopTime[toIndex] = from.animation.animationStopTime[fromIndex];
+
+    for (int f = 0; f < MAX_ANIMATION_FRAMES; f++)
+    {
+        to.animation.frame.src.x[toIndex][f] = from.animation.frame.src.x[fromIndex][f];
+        to.animation.frame.src.y[toIndex][f] = from.animation.frame.src.y[fromIndex][f];
+        to.animation.frame.src.w[toIndex][f] = from.animation.frame.src.w[fromIndex][f];
+        to.animation.frame.src.h[toIndex][f] = from.animation.frame.src.h[fromIndex][f];
+
+        to.animation.frame.anchor.initialOffX[toIndex][f] = from.animation.frame.anchor.initialOffX[fromIndex][f];
+        to.animation.frame.anchor.initialOffY[toIndex][f] = from.animation.frame.anchor.initialOffY[fromIndex][f];
+        to.animation.frame.anchor.initialW[toIndex][f] = from.animation.frame.anchor.initialW[fromIndex][f];
+        to.animation.frame.anchor.initialH[toIndex][f] = from.animation.frame.anchor.initialH[fromIndex][f];
+        to.animation.frame.anchor.offX[toIndex][f] = from.animation.frame.anchor.offX[fromIndex][f];
+        to.animation.frame.anchor.offY[toIndex][f] = from.animation.frame.anchor.offY[fromIndex][f];
+        to.animation.frame.anchor.w[toIndex][f] = from.animation.frame.anchor.w[fromIndex][f];
+        to.animation.frame.anchor.h[toIndex][f] = from.animation.frame.anchor.h[fromIndex][f];
+
+        to.animation.frame.collision.initialOffX[toIndex][f] = from.animation.frame.collision.initialOffX[fromIndex][f];
+        to.animation.frame.collision.initialOffY[toIndex][f] = from.animation.frame.collision.initialOffY[fromIndex][f];
+        to.animation.frame.collision.initialW[toIndex][f] = from.animation.frame.collision.initialW[fromIndex][f];
+        to.animation.frame.collision.initialH[toIndex][f] = from.animation.frame.collision.initialH[fromIndex][f];
+        to.animation.frame.collision.offX[toIndex][f] = from.animation.frame.collision.offX[fromIndex][f];
+        to.animation.frame.collision.offY[toIndex][f] = from.animation.frame.collision.offY[fromIndex][f];
+        to.animation.frame.collision.w[toIndex][f] = from.animation.frame.collision.w[fromIndex][f];
+        to.animation.frame.collision.h[toIndex][f] = from.animation.frame.collision.h[fromIndex][f];
+
+        to.animation.frame.frameDuration[toIndex][f] = from.animation.frame.frameDuration[fromIndex][f];
+    }
+
+    to.position.x[toIndex] = from.position.x[fromIndex];
+    to.position.y[toIndex] = from.position.y[fromIndex];
+    to.position.initialW[toIndex] = from.position.initialW[fromIndex];
+    to.position.initialH[toIndex] = from.position.initialH[fromIndex];
+    to.position.w[toIndex] = from.position.w[fromIndex];
+    to.position.h[toIndex] = from.position.h[fromIndex];
+
+    to.scale[toIndex] = from.scale[fromIndex];
+
+    to.rotation.rotate[toIndex] = from.rotation.rotate[fromIndex];
+    to.rotation.initialRotate[toIndex] = from.rotation.initialRotate[fromIndex];
+    to.rotation.initialAngle[toIndex] = from.rotation.initialAngle[fromIndex];
+
+    to.facing.facing[toIndex] = from.facing.facing[fromIndex];
+    to.facing.initialFacing[toIndex] = from.facing.initialFacing[fromIndex];
+    to.facing.dirty[toIndex] = from.facing.dirty[fromIndex];
+    to.facing.flipX[toIndex] = from.facing.flipX[fromIndex];
+}
