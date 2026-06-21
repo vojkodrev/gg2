f9a68639d4693166d016d7ff126c2136e188f40d
Vojko Drev <vojkodrev@gmail.com>
2026-06-21 09:15:47 +0700

    Only sync equipment facing when parent facing is dirty

--- gg2/systems/gameplay/flipx/EquipmentFacingSystem.cpp
+++ gg2/systems/gameplay/flipx/EquipmentFacingSystem.cpp
@@ -4,18 +4,24 @@
 
 void equipmentFacingSystem(Context &ctx)
 {
-    syncFlipFromParentFacingAndMarkDirty(
-        ctx.data.player.base.facing,
-        ctx.data.player.equipment.weapon.base.facing,
-        0);
+    if (ctx.data.player.base.facing.dirty[0])
+    {
+        syncFlipFromParentFacingAndMarkDirty(
+            ctx.data.player.base.facing,
+            ctx.data.player.equipment.weapon.base.facing,
+            0);
 
-    syncFlipFromParentFacingAndMarkDirty(
-        ctx.data.player.base.facing,
-        ctx.data.player.equipment.ammo.base.facing,
-        0);
+        syncFlipFromParentFacingAndMarkDirty(
+            ctx.data.player.base.facing,
+            ctx.data.player.equipment.ammo.base.facing,
+            0);
+    }
 
     for (uint32_t i = 0; i < ctx.data.npc.npcCount; i++)
     {
+        if (!ctx.data.npc.base.facing.dirty[i])
+            continue;
+
         syncFlipFromParentFacingAndMarkDirty(
             ctx.data.npc.base.facing,
             ctx.data.npc.equipment.weapon.base.facing,
