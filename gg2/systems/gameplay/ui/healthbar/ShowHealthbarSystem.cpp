15c9ec26b877d24020e9a5ade95107de47490099
Vojko Drev <vojkodrev@gmail.com>
2026-06-21 13:15:34 +0700

    Show player healthbar when damaged

--- gg2/systems/gameplay/ui/healthbar/ShowHealthbarSystem.cpp
+++ gg2/systems/gameplay/ui/healthbar/ShowHealthbarSystem.cpp
@@ -3,6 +3,9 @@
 
 void showHealthbarSystem(Context &ctx)
 {
+    ctx.data.player.healthbar.show[0] =
+        ctx.data.player.statistics.hp[0] < ctx.data.player.statistics.maxHp[0];
+
     for (uint32_t i = 0; i < ctx.data.npc.npcCount; i++)
         ctx.data.npc.healthbar.show[i] =
             ctx.data.npc.statistics.hp[i] < ctx.data.npc.statistics.maxHp[i];
