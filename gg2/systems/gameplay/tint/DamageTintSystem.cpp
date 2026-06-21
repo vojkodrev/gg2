9159952e07cb1dccda7d4e2cf9e01f58943c5287
Vojko Drev <vojkodrev@gmail.com>
2026-06-21 21:55:30 +0700

    Tint equipment on damage

--- gg2/systems/gameplay/tint/DamageTintSystem.cpp
+++ gg2/systems/gameplay/tint/DamageTintSystem.cpp
@@ -5,5 +5,10 @@
 void damageTintSystem(Context &ctx)
 {
     tintWhenHpDrops(ctx.data.player.base, ctx.data.player.statistics, 1);
+    tintWhenHpDrops(ctx.data.player.equipment.weapon.base, ctx.data.player.statistics, 1);
+    tintWhenHpDrops(ctx.data.player.equipment.ammo.base, ctx.data.player.statistics, 1);
+
     tintWhenHpDrops(ctx.data.npc.base, ctx.data.npc.statistics, ctx.data.npc.npcCount);
+    tintWhenHpDrops(ctx.data.npc.equipment.weapon.base, ctx.data.npc.statistics, ctx.data.npc.npcCount);
+    tintWhenHpDrops(ctx.data.npc.equipment.ammo.base, ctx.data.npc.statistics, ctx.data.npc.npcCount);
 }
