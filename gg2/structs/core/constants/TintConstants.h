f5e2a41c0766f4198c6d765d908558b1ea16e758
Vojko Drev <vojkodrev@gmail.com>
2026-06-21 21:28:40 +0700

    Tint npc when taking damage

--- gg2/structs/core/constants/TintConstants.h
+++ gg2/structs/core/constants/TintConstants.h
@@ -4,6 +4,7 @@ const float DAMAGE_TINT_R = 1.0f;
 const float DAMAGE_TINT_G = 0.0f;
 const float DAMAGE_TINT_B = 0.0f;
 const float DAMAGE_TINT_A = 1.0f;
+const float DAMAGE_TINT_CLEAR_TIME = 0.75f;
 
 const float CLEAR_TINT_R = 1.0f;
 const float CLEAR_TINT_G = 1.0f;
--- gg2/systems/gameplay/collision/effects/ResolveProjectileEffectCollision.cpp
+++ gg2/systems/gameplay/collision/effects/ResolveProjectileEffectCollision.cpp
@@ -1,6 +1,7 @@
 #include "ResolveProjectileEffectCollision.h"
 #include "ColIdIndex.h"
 #include "../../../structs/core/constants/ProjectileConstants.h"
+#include "../../../structs/core/constants/TintConstants.h"
 #include "../../../structs/npc/NPCAiType.h"
 #include "../../../utils/effects/EffectFree.h"
 #include <algorithm>
@@ -29,6 +30,14 @@ void resolveProjectileEffectCollision(
         const int nextHp = std::max(0, hp - PROJECTILE_DAMAGE);
         ctx.data.npc.statistics.hpDirty[npcIndex] = hp != nextHp;
         ctx.data.npc.statistics.hp[npcIndex] = nextHp;
+        if (hp != nextHp)
+        {
+            ctx.data.npc.base.tint.r[npcIndex] = DAMAGE_TINT_R;
+            ctx.data.npc.base.tint.g[npcIndex] = DAMAGE_TINT_G;
+            ctx.data.npc.base.tint.b[npcIndex] = DAMAGE_TINT_B;
+            ctx.data.npc.base.tint.a[npcIndex] = DAMAGE_TINT_A;
+            ctx.data.npc.base.tint.clearTimer[npcIndex] = DAMAGE_TINT_CLEAR_TIME;
+        }
         effectFree(effect, ctx.data.groups, effectIndex);
     }
 }
