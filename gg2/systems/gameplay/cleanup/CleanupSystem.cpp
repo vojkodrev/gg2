50ad4c02400f7ad0a284c220731dc243a3b94439
Vojko Drev <vojkodrev@gmail.com>
2026-06-22 18:57:12 +0700

    Inline dirty flag cleanup

--- gg2/systems/gameplay/cleanup/CleanupSystem.cpp
+++ gg2/systems/gameplay/cleanup/CleanupSystem.cpp
@@ -1,21 +1,31 @@
 #include "CleanupSystem.h"
-#include "CleanupEntityBase.h"
-#include "CleanupStatistics.h"
+#include <cstring>
 
 void cleanupSystem(Context &ctx)
 {
-    cleanupEntityBase(ctx.data.player.base, 1);
-    cleanupEntityBase(ctx.data.player.equipment.weapon.base, 1);
-    cleanupEntityBase(ctx.data.player.equipment.ammo.base, 1);
-    cleanupEntityBase(ctx.data.player.healthbar.base, 1);
-    cleanupStatistics(ctx.data.player.statistics, 1);
+    std::memset(ctx.data.player.base.position.dirty, 0, sizeof(ctx.data.player.base.position.dirty));
+    std::memset(ctx.data.player.base.facing.dirty, 0, sizeof(ctx.data.player.base.facing.dirty));
+    std::memset(ctx.data.player.equipment.weapon.base.position.dirty, 0, sizeof(ctx.data.player.equipment.weapon.base.position.dirty));
+    std::memset(ctx.data.player.equipment.weapon.base.facing.dirty, 0, sizeof(ctx.data.player.equipment.weapon.base.facing.dirty));
+    std::memset(ctx.data.player.equipment.ammo.base.position.dirty, 0, sizeof(ctx.data.player.equipment.ammo.base.position.dirty));
+    std::memset(ctx.data.player.equipment.ammo.base.facing.dirty, 0, sizeof(ctx.data.player.equipment.ammo.base.facing.dirty));
+    std::memset(ctx.data.player.healthbar.base.position.dirty, 0, sizeof(ctx.data.player.healthbar.base.position.dirty));
+    std::memset(ctx.data.player.healthbar.base.facing.dirty, 0, sizeof(ctx.data.player.healthbar.base.facing.dirty));
+    std::memset(ctx.data.player.statistics.hpDirty, 0, sizeof(ctx.data.player.statistics.hpDirty));
 
-    cleanupEntityBase(ctx.data.npc.base, ctx.data.npc.npcCount);
-    cleanupEntityBase(ctx.data.npc.equipment.weapon.base, ctx.data.npc.npcCount);
-    cleanupEntityBase(ctx.data.npc.equipment.ammo.base, ctx.data.npc.npcCount);
-    cleanupEntityBase(ctx.data.npc.healthbar.base, ctx.data.npc.npcCount);
-    cleanupStatistics(ctx.data.npc.statistics, ctx.data.npc.npcCount);
+    std::memset(ctx.data.npc.base.position.dirty, 0, sizeof(ctx.data.npc.base.position.dirty));
+    std::memset(ctx.data.npc.base.facing.dirty, 0, sizeof(ctx.data.npc.base.facing.dirty));
+    std::memset(ctx.data.npc.equipment.weapon.base.position.dirty, 0, sizeof(ctx.data.npc.equipment.weapon.base.position.dirty));
+    std::memset(ctx.data.npc.equipment.weapon.base.facing.dirty, 0, sizeof(ctx.data.npc.equipment.weapon.base.facing.dirty));
+    std::memset(ctx.data.npc.equipment.ammo.base.position.dirty, 0, sizeof(ctx.data.npc.equipment.ammo.base.position.dirty));
+    std::memset(ctx.data.npc.equipment.ammo.base.facing.dirty, 0, sizeof(ctx.data.npc.equipment.ammo.base.facing.dirty));
+    std::memset(ctx.data.npc.healthbar.base.position.dirty, 0, sizeof(ctx.data.npc.healthbar.base.position.dirty));
+    std::memset(ctx.data.npc.healthbar.base.facing.dirty, 0, sizeof(ctx.data.npc.healthbar.base.facing.dirty));
+    std::memset(ctx.data.npc.statistics.hpDirty, 0, sizeof(ctx.data.npc.statistics.hpDirty));
 
-    cleanupEntityBase(ctx.data.object.base, ctx.data.object.objectCount);
-    cleanupEntityBase(ctx.data.effect.base, ctx.data.effect.pool.count);
+    std::memset(ctx.data.object.base.position.dirty, 0, sizeof(ctx.data.object.base.position.dirty));
+    std::memset(ctx.data.object.base.facing.dirty, 0, sizeof(ctx.data.object.base.facing.dirty));
+
+    std::memset(ctx.data.effect.base.position.dirty, 0, sizeof(ctx.data.effect.base.position.dirty));
+    std::memset(ctx.data.effect.base.facing.dirty, 0, sizeof(ctx.data.effect.base.facing.dirty));
 }
