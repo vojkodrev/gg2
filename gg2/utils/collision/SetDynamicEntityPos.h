66741d6c7f441f6d015086e488c2acc2cafcc5c6
Vojko Drev <vojkodrev@gmail.com>
2026-06-22 18:56:25 +0700

    Preserve dirty position during collision resolution

--- gg2/utils/collision/SetDynamicEntityPos.h
+++ gg2/utils/collision/SetDynamicEntityPos.h
@@ -8,6 +8,7 @@ inline void setDynamicEntityPos(Context &ctx, uint32_t id, SDL_FPoint pos)
     if (colIdType(id) == ColType::Player)
     {
         ctx.data.player.base.position.dirty[0] =
+            ctx.data.player.base.position.dirty[0] ||
             ctx.data.player.base.position.x[0] != pos.x ||
             ctx.data.player.base.position.y[0] != pos.y;
         ctx.data.player.base.position.x[0] = pos.x;
@@ -17,6 +18,7 @@ inline void setDynamicEntityPos(Context &ctx, uint32_t id, SDL_FPoint pos)
     {
         uint32_t i = colIdIndex(id);
         ctx.data.npc.base.position.dirty[i] =
+            ctx.data.npc.base.position.dirty[i] ||
             ctx.data.npc.base.position.x[i] != pos.x ||
             ctx.data.npc.base.position.y[i] != pos.y;
         ctx.data.npc.base.position.x[i] = pos.x;
