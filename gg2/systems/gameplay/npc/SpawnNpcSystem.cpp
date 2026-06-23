4ea23b66aa41aeca00dc46b08580e3f52e36ed65
Vojko Drev <vojkodrev@gmail.com>
2026-06-23 15:25:43 +0700

    Reset npc state on respawn

--- gg2/systems/gameplay/npc/SpawnNpcSystem.cpp
+++ gg2/systems/gameplay/npc/SpawnNpcSystem.cpp
@@ -1,5 +1,7 @@
 #include "SpawnNpcSystem.h"
 #include "../../../structs/core/constants/NpcConstants.h"
+#include "../../../structs/core/constants/TintConstants.h"
+#include "../ai/SetNpcAiStateIdle.h"
 
 void spawnNpcSystem(Context &ctx)
 {
@@ -25,7 +27,16 @@ void spawnNpcSystem(Context &ctx)
             ctx.data.npc.statistics.prevHp[i] = ctx.data.npc.statistics.maxHp[i];
             ctx.data.npc.statistics.hp[i] = ctx.data.npc.statistics.maxHp[i];
             ctx.data.npc.statistics.hpDirty[i] = true;
+            setNpcAiStateIdle(i, ctx);
+            ctx.data.npc.ai.path.length[i] = 0;
+            ctx.data.npc.ai.path.index[i] = 0;
+            ctx.data.npc.ai.repathTimer[i] = 0.0f;
             ctx.data.npc.ai.patrol.index[i] = 0;
+            ctx.data.npc.base.tint.r[i] = CLEAR_TINT_R;
+            ctx.data.npc.base.tint.g[i] = CLEAR_TINT_G;
+            ctx.data.npc.base.tint.b[i] = CLEAR_TINT_B;
+            ctx.data.npc.base.tint.a[i] = CLEAR_TINT_A;
+            ctx.data.npc.base.tint.clearTimer[i] = 0.0f;
             ctx.data.npc.base.position.x[i] = ctx.data.npc.ai.spawn.x[i];
             ctx.data.npc.base.position.y[i] = ctx.data.npc.ai.spawn.y[i];
             ctx.data.npc.base.position.dirty[i] = true;
