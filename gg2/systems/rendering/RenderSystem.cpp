2c9fed5f07a862c98f459c04550878a8d5aaa6a3
Vojko Drev <vojkodrev@gmail.com>
2026-06-29 13:50:54 +0700

    Fix render seams with pixel-art scaling and snapped positions

--- gg2/systems/rendering/RenderSystem.cpp
+++ gg2/systems/rendering/RenderSystem.cpp
@@ -1,6 +1,7 @@
 #include "RenderSystem.h"
 #include "debug/DebugRenderSystem.h"
 #include <SDL3/SDL.h>
+#include <cmath>
 
 void renderSystem(const Context &ctx)
 {
@@ -8,12 +9,17 @@ void renderSystem(const Context &ctx)
     auto &tileMap = ctx.data.tileMap;
     const SDL_FPoint off = ctx.data.camera.offset;
     const SDL_FRect screen = ctx.data.camera.screen;
-    SDL_SetTextureScaleMode(ctx.texture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(ctx.texture, SDL_SCALEMODE_PIXELART);
     SDL_SetRenderDrawColor(ctx.renderer, 0, 0, 0, 255);
     SDL_RenderClear(ctx.renderer);
     for (uint32_t i = 0; i < tileMap.tileCount; i++)
     {
-        SDL_FRect dst = {(float)tileMap.tiles.dstX[i] + off.x, (float)tileMap.tiles.dstY[i] + off.y, (float)props.dstTileW, (float)props.dstTileH};
+        SDL_FRect dst = {
+            std::round((float)tileMap.tiles.dstX[i] + off.x),
+            std::round((float)tileMap.tiles.dstY[i] + off.y),
+            (float)props.dstTileW,
+            (float)props.dstTileH
+        };
         if (!SDL_HasRectIntersectionFloat(&dst, &screen))
             continue;
         SDL_FRect src = {(float)tileMap.tiles.srcX[i], (float)tileMap.tiles.srcY[i], (float)props.srcTileW, (float)props.srcTileH};
@@ -23,7 +29,12 @@ void renderSystem(const Context &ctx)
     for (uint32_t i = 0; i < rb.count; i++)
     {
         SDL_FRect src = {rb.src.x[i], rb.src.y[i], rb.src.w[i], rb.src.h[i]};
-        SDL_FRect dst = {rb.dst.x[i], rb.dst.y[i], rb.dst.w[i], rb.dst.h[i]};
+        SDL_FRect dst = {
+            std::round(rb.dst.x[i]),
+            std::round(rb.dst.y[i]),
+            rb.dst.w[i],
+            rb.dst.h[i]
+        };
         SDL_FPoint rotationCenter = {rb.rotationCenter.point.x[i], rb.rotationCenter.point.y[i]};
         SDL_FPoint *center = rb.rotationCenter.hasCenter[i] ? &rotationCenter : nullptr;
         SDL_FlipMode flip = rb.flipX[i] ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
