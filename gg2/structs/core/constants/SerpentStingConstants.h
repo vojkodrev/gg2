ccbc908a0a61f7124a197eb7164467eec21ca222
Vojko Drev <vojkodrev@gmail.com>
2026-07-01 11:46:49 +0700

    Add serpent sting mana cost and low-mana action bar tint

--- gg2/structs/core/constants/SerpentStingConstants.h
+++ gg2/structs/core/constants/SerpentStingConstants.h
@@ -3,3 +3,4 @@
 constexpr float SERPENT_STING_DEBUFF_TIME = 10.0f;
 constexpr int SERPENT_STING_TICK_COUNT = 6;
 constexpr int SERPENT_STING_DAMAGE = 40;
+constexpr int SERPENT_STING_MANA_COST = 200;
