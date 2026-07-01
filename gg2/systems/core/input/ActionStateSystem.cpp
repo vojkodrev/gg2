92107aa125497d8f2b94ac0a92b937a0a8385ab1
Vojko Drev <vojkodrev@gmail.com>
2026-07-01 15:09:59 +0700

    Add digit3 release tracking

--- gg2/systems/core/input/ActionStateSystem.cpp
+++ gg2/systems/core/input/ActionStateSystem.cpp
@@ -6,15 +6,18 @@ void actionStateSystem(Context &ctx)
     const bool tabDown = ctx.keyboard.tab;
     const bool digit1Down = ctx.keyboard.digit1;
     const bool digit2Down = ctx.keyboard.digit2;
+    const bool digit3Down = ctx.keyboard.digit3;
     const bool mouseLeftDown = ctx.mouse.leftClicked;
 
     action.tabReleased = action.prevTabDown && !tabDown;
     action.digit1Released = action.prevDigit1Down && !digit1Down;
     action.digit2Released = action.prevDigit2Down && !digit2Down;
+    action.digit3Released = action.prevDigit3Down && !digit3Down;
     action.mouseLeftReleased = action.prevMouseLeftDown && !mouseLeftDown;
 
     action.prevTabDown = tabDown;
     action.prevDigit1Down = digit1Down;
     action.prevDigit2Down = digit2Down;
+    action.prevDigit3Down = digit3Down;
     action.prevMouseLeftDown = mouseLeftDown;
 }
