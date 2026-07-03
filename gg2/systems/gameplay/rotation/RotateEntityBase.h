43dc72c44759401fd08dc3b8dcd92a4f163b9c4d
Vojko Drev <vojkodrev@gmail.com>
2026-07-03 16:37:08 +0700

    Use running rotation animations for anchor-centered rotation and clean up rotation helpers.

--- gg2/systems/gameplay/rotation/RotateEntityBase.h
+++ gg2/systems/gameplay/rotation/RotateEntityBase.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "RotateEntityAnchorsAndCollision.h"
+#include "IsRotationAnimationRunning.h"
 #include "../../../structs/core/EntityBase.h"
+#include "../../../utils/collision/EntityColCenter.h"
+#include "../../../utils/rect/RotateRectCenter.h"
 #include <cstdint>
 
 template<int N>
@@ -9,9 +11,50 @@ inline void rotateEntityBase(EntityBase<N> &entityBase, uint32_t i)
     if (entityBase.animation.frameCount[i] == 0)
         return;
 
-    entityBase.rotation.rotate[i] = entityBase.facing.flipX[i]
-        ? -entityBase.rotation.initialRotate[i]
-        : entityBase.rotation.initialRotate[i];
+    const bool useAnimatedRotation = isRotationAnimationRunning(entityBase, i);
+    entityBase.rotation.rotate[i] = useAnimatedRotation
+        ? entityBase.animation.rotate[i]
+        : entityBase.facing.flipX[i]
+            ? -entityBase.rotation.initialRotate[i]
+            : entityBase.rotation.initialRotate[i];
 
-    rotateEntityAnchorsAndCollision(entityBase, i);
+    if (useAnimatedRotation)
+    {
+        const int frameIndex = entityBase.animation.frameIndex[i];
+        const SDL_FPoint center =
+            entityColCenter(entityBase.animation.frame.anchor, i, frameIndex);
+        entityBase.rotation.center.point.x[i] = center.x;
+        entityBase.rotation.center.point.y[i] = center.y;
+        entityBase.rotation.center.hasCenter[i] = true;
+    }
+    else
+    {
+        entityBase.rotation.center.hasCenter[i] = false;
+    }
+
+    const float entityCenterX = entityBase.position.w[i] * 0.5f;
+    const float entityCenterY = entityBase.position.h[i] * 0.5f;
+    for (int f = 0; f < entityBase.animation.frameCount[i]; f++)
+    {
+        const SDL_FPoint center = useAnimatedRotation
+            ? entityColCenter(entityBase.animation.frame.anchor, i, f)
+            : SDL_FPoint{entityCenterX, entityCenterY};
+        if (!useAnimatedRotation)
+            rotateRectCenter(
+                entityBase.animation.frame.anchor.offX[i][f],
+                entityBase.animation.frame.anchor.offY[i][f],
+                entityBase.animation.frame.anchor.w[i][f],
+                entityBase.animation.frame.anchor.h[i][f],
+                center.x,
+                center.y,
+                entityBase.rotation.rotate[i]);
+        rotateRectCenter(
+            entityBase.animation.frame.collision.offX[i][f],
+            entityBase.animation.frame.collision.offY[i][f],
+            entityBase.animation.frame.collision.w[i][f],
+            entityBase.animation.frame.collision.h[i][f],
+            center.x,
+            center.y,
+            entityBase.rotation.rotate[i]);
+    }
 }
--- gg2/systems/gameplay/rotation/RotateEquipmentSystem.cpp
+++ gg2/systems/gameplay/rotation/RotateEquipmentSystem.cpp
@@ -1,25 +1,29 @@
 #include "RotateEquipmentSystem.h"
+#include "IsRotationAnimationRunning.h"
 #include "RotateEntityBase.h"
-#include "RotateAnchorAroundEntityCenter.h"
+#include "../../../utils/rect/RotateRectCenter.h"
 #include <cstdint>
 
 void rotateEquipmentSystem(Context &ctx)
 {
     auto &playerWeapon = ctx.data.player.equipment.weapon;
-    if (playerWeapon.base.facing.dirty[0])
+    if (playerWeapon.base.facing.dirty[0] || isRotationAnimationRunning(playerWeapon.base, 0))
     {
         rotateEntityBase(playerWeapon.base, 0);
         if (playerWeapon.type[0] == WeaponType::Ranged)
-            rotateAnchorAroundEntityCenter(
-                playerWeapon.ammoAnchor,
-                playerWeapon.base,
-                0,
-                0);
+            rotateRectCenter(
+                playerWeapon.ammoAnchor.offX[0][0],
+                playerWeapon.ammoAnchor.offY[0][0],
+                playerWeapon.ammoAnchor.w[0][0],
+                playerWeapon.ammoAnchor.h[0][0],
+                playerWeapon.base.position.w[0] * 0.5f,
+                playerWeapon.base.position.h[0] * 0.5f,
+                playerWeapon.base.rotation.rotate[0]);
     }
 
     auto &playerAmmo = ctx.data.player.equipment.ammo;
     if (
-        playerAmmo.base.facing.dirty[0] &&
+        (playerAmmo.base.facing.dirty[0] || isRotationAnimationRunning(playerAmmo.base, 0)) &&
         playerWeapon.type[0] == WeaponType::Ranged)
         rotateEntityBase(playerAmmo.base, 0);
 
@@ -30,18 +34,21 @@ void rotateEquipmentSystem(Context &ctx)
         if (!ctx.data.npc.active[i])
             continue;
 
-        if (npcWeapon.base.facing.dirty[i])
+        if (npcWeapon.base.facing.dirty[i] || isRotationAnimationRunning(npcWeapon.base, i))
         {
             rotateEntityBase(npcWeapon.base, i);
             if (npcWeapon.type[i] == WeaponType::Ranged)
-                rotateAnchorAroundEntityCenter(
-                    npcWeapon.ammoAnchor,
-                    npcWeapon.base,
-                    i,
-                    0);
+                rotateRectCenter(
+                    npcWeapon.ammoAnchor.offX[i][0],
+                    npcWeapon.ammoAnchor.offY[i][0],
+                    npcWeapon.ammoAnchor.w[i][0],
+                    npcWeapon.ammoAnchor.h[i][0],
+                    npcWeapon.base.position.w[i] * 0.5f,
+                    npcWeapon.base.position.h[i] * 0.5f,
+                    npcWeapon.base.rotation.rotate[i]);
         }
         if (
-            npcAmmo.base.facing.dirty[i] &&
+            (npcAmmo.base.facing.dirty[i] || isRotationAnimationRunning(npcAmmo.base, i)) &&
             npcWeapon.type[i] == WeaponType::Ranged)
             rotateEntityBase(npcAmmo.base, i);
     }
