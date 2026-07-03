e47491ab5b33c73ea0cc772f90aaa99784312356
Vojko Drev <vojkodrev@gmail.com>
2026-07-03 17:48:34 +0700

    Start melee monster swing animations when in range and add their attack delay constant.

--- gg2/systems/gameplay/ai/monster/MonsterAttack.cpp
+++ gg2/systems/gameplay/ai/monster/MonsterAttack.cpp
@@ -1,6 +1,9 @@
 #include "MonsterAttack.h"
 #include "AreColBoxesNear.h"
 #include "GetEntityColAABB.h"
+#include "../../../../structs/core/AnimationState.h"
+#include "../../../../structs/core/constants/NpcMonsterConstants.h"
+#include "../../../../structs/equipment/WeaponType.h"
 #include "SetNpcAiStateReturnToSpawn.h"
 #include "SetNpcAiStatePursueTarget.h"
 #include "CanMonsterAttackTarget.h"
@@ -17,5 +20,18 @@ void monsterAttack(uint32_t n, Context &ctx)
         return;
     const SDL_FRect targetCol = getEntityColAABB(ctx, target.type[n], target.id[n]);
     if (!areColBoxesNear(ctx, n, targetCol, NPC_ATTACK_REACH))
+    {
         setNpcAiStatePursueTarget(n, ctx, target.type[n], target.id[n]);
+        return;
+    }
+
+    auto &npc = ctx.data.npc;
+    if (npc.equipment.weapon.type[n] != WeaponType::Melee)
+        return;
+
+    if (npc.autoAttack.attackTimer[n] > 0.0f)
+        return;
+
+    npc.autoAttack.attackTimer[n] = NPC_MELEE_AUTO_ATTACK_DELAY;
+    npc.equipment.weapon.base.animation.animationState[n] = AnimationState::Starting;
 }
