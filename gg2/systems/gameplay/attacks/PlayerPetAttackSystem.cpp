2856abd9a8c7615f95b7c6b228533a011b68fa35
vojkodrev <vojkodrev@gmail.com>
2026-07-09 19:42:33 +0700

    Remove pet follow system and idle pets on auto-attack toggle off

--- gg2/systems/gameplay/attacks/PlayerPetAttackSystem.cpp
+++ gg2/systems/gameplay/attacks/PlayerPetAttackSystem.cpp
@@ -1,4 +1,5 @@
 #include "PlayerPetAttackSystem.h"
+#include "../ai/SetNpcAiStateIdle.h"
 #include "../ai/monster/SetNpcAiStatePursueTarget.h"
 #include "../../../structs/core/EntityType.h"
 #include "../../../structs/npc/NPCAiType.h"
@@ -12,12 +13,25 @@ void playerPetAttackSystem(Context &ctx)
     if (petId == -1)
         return;
 
+    auto &petAutoAttackActive = ctx.data.npc.autoAttack.active[petId];
+    petAutoAttackActive = !petAutoAttackActive;
+    if (!petAutoAttackActive)
+    {
+        setNpcAiStateIdle((uint32_t)petId, ctx);
+        return;
+    }
+
     const int targetId = ctx.data.player.selectedNpc;
     if (targetId == -1)
+    {
+        petAutoAttackActive = false;
         return;
+    }
     if (ctx.data.npc.ai.type[targetId] != NPCAiType::Monster)
+    {
+        petAutoAttackActive = false;
         return;
+    }
 
-    ctx.data.npc.autoAttack.active[petId] = true;
     setNpcAiStatePursueTarget((uint32_t)petId, ctx, EntityType::NPC, targetId);
 }
