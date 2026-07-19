20e6d00e22001da57fb35538572c256ac100f8de
vojkodrev <vojkodrev@gmail.com>
2026-07-19 16:36:53 +0700

    Face player toward selected target

--- gg2/systems/gameplay/PlayerFacingSystem.cpp
+++ gg2/systems/gameplay/PlayerFacingSystem.cpp
@@ -1,10 +1,19 @@
 #include "PlayerFacingSystem.h"
 #include "SetFacingTowardX.h"
+#include "../../structs/core/constants/IndexConstants.h"
+#include "../../utils/collision/EntityColAABB.h"
+#include "../../utils/collision/EntityColCenter.h"
 #include "../../utils/rect/EntityPositionCenter.h"
 
 void playerFacingSystem(Context &ctx)
 {
     auto &p = ctx.data.player;
-    SDL_FPoint center = entityPositionCenter(p.base.position, 0);
-    setFacingTowardX(p.base.facing, 0, center.x, ctx.mouse.worldX);
+    const SDL_FPoint center = entityPositionCenter(p.base.position, 0);
+    float facingTargetX = ctx.mouse.worldX;
+
+    const int selectedNpc = p.selectedNpc;
+    if (selectedNpc != INVALID_ID && ctx.data.npc.active[selectedNpc])
+        facingTargetX = entityColCenter(entityColAABB(ctx.data.npc.base, selectedNpc)).x;
+
+    setFacingTowardX(p.base.facing, 0, center.x, facingTargetX);
 }
