048ab38d6d0ef04f20aea4d934450e355fac0a1f
vojkodrev <vojkodrev@gmail.com>
2026-07-24 11:00:27 +0700

    Show monster aggro range in debug overlay

--- gg2/systems/rendering/debug/DebugRenderRangedDeadZone.cpp
+++ gg2/systems/rendering/debug/DebugRenderRangedDeadZone.cpp
@@ -1,9 +1,11 @@
 #include "DebugRenderRangedDeadZone.h"
 #include "EntityColAABB.h"
 #include "EntityColCenter.h"
+#include "RenderMonsterAggroRange.h"
 #include "RenderNpcTargetVisibleLine.h"
 #include "RenderRangedDeadZone.h"
 #include "../../../structs/equipment/WeaponType.h"
+#include "../../../structs/npc/NPCAiType.h"
 
 void debugRenderRangedDeadZone(const Context &ctx)
 {
@@ -21,14 +23,27 @@ void debugRenderRangedDeadZone(const Context &ctx)
     const auto &npc = ctx.data.npc;
     for (uint32_t i = 0; i < MAX_NPCS; i++)
     {
-        if (!npc.active[i] || npc.equipment.weapon.type[i] != WeaponType::Ranged)
+        if (!npc.active[i])
             continue;
 
-        renderRangedDeadZone(
-            ctx,
-            entityColCenter(entityColAABB(npc.base, i)));
+        const SDL_FPoint npcCenter =
+            entityColCenter(entityColAABB(npc.base, i));
+        if (npc.ai.type[i] == NPCAiType::MonsterMelee ||
+            npc.ai.type[i] == NPCAiType::MonsterRanged)
+        {
+            SDL_SetRenderDrawColor(ctx.renderer, 255, 0, 0, 255);
+            renderMonsterAggroRange(ctx, npcCenter);
+        }
 
-        if (npc.equipment.weapon.showAmmo[i] && npc.ai.targetVisible[i])
-            renderNpcTargetVisibleLine(ctx, i);
+        if (npc.equipment.weapon.type[i] == WeaponType::Ranged)
+        {
+            SDL_SetRenderDrawColor(ctx.renderer, 255, 128, 0, 255);
+            renderRangedDeadZone(
+                ctx,
+                npcCenter);
+
+            if (npc.equipment.weapon.showAmmo[i] && npc.ai.targetVisible[i])
+                renderNpcTargetVisibleLine(ctx, i);
+        }
     }
 }
