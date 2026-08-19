59b1d1a8d4f7846c86085c2cde19bcac707c0432
vojkodrev <vojkodrev@gmail.com>
2026-08-19 15:51:21 +0200

    Classify staff weapons as magic

--- gg2/structs/equipment/WeaponType.h
+++ gg2/structs/equipment/WeaponType.h
@@ -4,4 +4,5 @@ enum class WeaponType
 {
     Melee,
     Ranged,
+    Magic,
 };
--- gg2/systems/tilemap/ParseWeaponType.h
+++ gg2/systems/tilemap/ParseWeaponType.h
@@ -10,9 +10,14 @@ inline bool parseWeaponType(const std::string &weaponAssetType, WeaponType &weap
         return true;
     }
 
+    if (weaponAssetType.rfind("staff", 0) == 0)
+    {
+        weaponType = WeaponType::Magic;
+        return true;
+    }
+
     if (
         weaponAssetType.rfind("polearm", 0) == 0 ||
-        weaponAssetType.rfind("staff", 0) == 0 ||
         weaponAssetType.rfind("sword", 0) == 0)
     {
         weaponType = WeaponType::Melee;
