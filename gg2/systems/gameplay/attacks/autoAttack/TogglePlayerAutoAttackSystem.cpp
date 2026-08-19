49a144b6c3b5a991fc45a3e9fb57e8d23112486a
vojkodrev <vojkodrev@gmail.com>
2026-08-19 15:51:34 +0200

    Restrict auto attacks to physical weapons

--- gg2/systems/gameplay/attacks/autoAttack/TogglePlayerAutoAttackSystem.cpp
+++ gg2/systems/gameplay/attacks/autoAttack/TogglePlayerAutoAttackSystem.cpp
@@ -1,9 +1,16 @@
 #include "TogglePlayerAutoAttackSystem.h"
 #include "../../../structs/core/constants/IndexConstants.h"
+#include "../../../structs/equipment/WeaponType.h"
 
 void togglePlayerAutoAttackSystem(Context &ctx)
 {
     if (!ctx.data.action.digit1Released)
+        return;
+
+    const auto &weapon = ctx.data.player.equipment.weapon;
+    if (!weapon.exists[0] ||
+        (weapon.type[0] != WeaponType::Melee &&
+         weapon.type[0] != WeaponType::Ranged))
         return;
 
     auto &autoAttack = ctx.data.player.autoAttack;
